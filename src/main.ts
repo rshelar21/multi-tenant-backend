4b00327433a06db1856ee206ecbb1abd586f505b
rshelar21 <rshelar796@gmail.com>
2025-07-29 18:51:13 +0530

    fix: cors origins

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,20 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(helmet());
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      const allowedRoot = 'https://funroad.site';
+      const subdomainRegex = /^https:\/\/([a-z0-9-]+)\.funroad\.site$/;
+
+      if (
+        !origin || // allow requests without origin (e.g., Postman, curl)
+        origin === allowedRoot ||
+        subdomainRegex.test(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,POST,PUT,DELETE,PATCH', // Allow HTTP methods
     credentials: true, // Allow cookies to be sent
     optionsSuccessStatus: 200,
